b655bc5112d24961358a5863a456bd69ca56bac7
lhw-1 <lhw-1@users.noreply.github.com>
2025-02-17 14:49:25 +0000

    deploy: source-academy/frontend@98d605298ddef164543a3907a95581715302e98d

---
+++ static/js/date-fns-locale-et-_lib-formatRelative-index-js.65990e98.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkfrontend=globalThis.webpackChunkfrontend||[]).push([[74248],{58492:(e,l)=>{Object.defineProperty(l,"__esModule",{value:!0}),l.default=void 0;var t={lastWeek:"'eelmine' eeee 'kell' p",yesterday:"'eile kell' p",today:"'t\xe4na kell' p",tomorrow:"'homme kell' p",nextWeek:"'j\xe4rgmine' eeee 'kell' p",other:"P"},o=function(e,l,o,n){return t[e]};l.default=o,e.exports=l.default}}]);
+//# sourceMappingURL=date-fns-locale-et-_lib-formatRelative-index-js.65990e98.chunk.js.map
