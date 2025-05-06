6f135ab3ad83a44ff35e064222bd3beb29575f22
s-kybound <s-kybound@users.noreply.github.com>
2025-05-06 03:36:32 +0000

    deploy: source-academy/frontend@2f5bc86a47f2befc526a5c39638d323a054428e3

---
+++ static/js/date-fns-locale-ms-_lib-formatRelative-index-js.918997b1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkfrontend=globalThis.webpackChunkfrontend||[]).push([[69545],{83793:(e,a)=>{Object.defineProperty(a,"__esModule",{value:!0}),a.default=void 0;var t={lastWeek:"eeee 'lepas pada jam' p",yesterday:"'Semalam pada jam' p",today:"'Hari ini pada jam' p",tomorrow:"'Esok pada jam' p",nextWeek:"eeee 'pada jam' p",other:"P"},p=function(e,a,p,d){return t[e]};a.default=p,e.exports=a.default}}]);
+//# sourceMappingURL=date-fns-locale-ms-_lib-formatRelative-index-js.918997b1.chunk.js.map
