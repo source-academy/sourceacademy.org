00ba15a0a570f564a376236c784dbf547b2c3d0b
martin-henz <martin-henz@users.noreply.github.com>
2025-06-13 05:58:01 +0000

    deploy: source-academy/frontend@b9bb1cd0f6cacb1ed9aa37703f90d47c10993c96

---
+++ static/js/17643.3bd0f54f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkfrontend=globalThis.webpackChunkfrontend||[]).push([[17643],{17643:(e,t,a)=>{Object.defineProperty(t,"__esModule",{value:!0});const s=a(11346),n={"newArray(Ljava/lang/Class;I)Ljava/lang/Object;":(e,t)=>{const a=t[0].getNativeField("classRef"),n=t[1],r="["+a.getDescriptor(),l=a.getLoader().getClass(r);if(l.status===s.ResultType.ERROR)return void e.throwNewException("java/lang/ClassNotFoundException",l.msg);const o=l.result.instantiate();o.initArray(n),e.returnStackFrame(o)}};t.default=n}}]);
+//# sourceMappingURL=17643.3bd0f54f.chunk.js.map
