e1dea5027653ca3e20abd0bdd740959d852bfd38
sayomaki <sayomaki@users.noreply.github.com>
2026-04-10 18:17:56 +0000

    deploy: source-academy/frontend@b93fd820f442a57eb0e2402e040fccddfbd0a31d

---
+++ static/js/async/date-fns-locale-ar-SA-_lib-formatRelative-index-js.bf75de07.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkfrontend=globalThis.webpackChunkfrontend||[]).push([["52562"],{1272(e,t){Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var o={lastWeek:"'أخر' eeee 'عند' p",yesterday:"'أمس عند' p",today:"'اليوم عند' p",tomorrow:"'غداً عند' p",nextWeek:"eeee 'عند' p",other:"P"};t.default=function(e,t,r,a){return o[e]},e.exports=t.default}}]);
+//# sourceMappingURL=date-fns-locale-ar-SA-_lib-formatRelative-index-js.bf75de07.js.map
